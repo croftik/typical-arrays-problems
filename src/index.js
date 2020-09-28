b6024cc954815f34d68f81e0fc404a309f300af2
croftik <Chayka_Lory@mail.ru>
2020-09-28 23:21:33 +0300

    feat: complete all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  let numberMin = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (array[i] < numberMin){
+      numberMin = array[i];
+    }
+  }
+  return numberMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  let numberMax = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (array[i] > numberMax){
+      numberMax = array[i];
+    }
+  }
+  return numberMax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  let summa = array.reduce(function(a, b){ return a + b; });
+  return result = summa / array.length;
 }
